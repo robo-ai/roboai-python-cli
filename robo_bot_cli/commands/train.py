98caaf1c53f7f02f07e0235bfa1227b6b9bcf07b
martasls <martasls95@gmail.com>
2020-12-29 18:06:17 +0000

    Add force option to train command

--- robo_bot_cli/commands/train.py
+++ robo_bot_cli/commands/train.py
@@ -6,18 +6,19 @@ from datetime import datetime
 from robo_bot_cli.util.cli import print_info
 
 
-@click.command(name='train', help='Train Rasa models for the required bots.')
-@click.argument('languages', nargs=-1,)
-@click.option('--path', default='.', type=click.Path(), help="Directory of your bot to be trained.")
-@click.option('--dev-config', default='config.yml', type=str,
+@click.command(name="train", help="Train Rasa models for the required bots.")
+@click.argument("languages", nargs=-1,)
+@click.option("--path", default=".", type=click.Path(), help="Directory of your bot to be trained.")
+@click.option("--dev-config", default="config.yml", type=str,
               help="Name of the config file to be used. If this is not passed, default 'config.yml' is used.")
-@click.option('--nlu', '-n', 'nlu', is_flag=True,
+@click.option("--nlu", "-n", "nlu", is_flag=True,
               help="Train exclusively the RASA nlu model for the given languages")
-@click.option('--core', '-c', 'core', is_flag=True,
+@click.option("--core", "-c", "core", is_flag=True,
               help="Train exclusively the RASA core model for the given languages")
-@click.option('--augmentation', 'augmentation', default=50, help="How much data augmentation to use during training. \
+@click.option("--augmentation", "augmentation", default=50, help="How much data augmentation to use during training. \
               (default: 50)")
-def command(languages: tuple, path: str, dev_config: str, nlu: bool, core: bool, augmentation: int):
+@click.option("--force", "force", is_flag=True, default=False, help="Force a model training even if the data has not changed. (default: False)")
+def command(languages: tuple, path: str, dev_config: str, nlu: bool, core: bool, augmentation: int, force: bool):
     """
     Wrapper of rasa train for multi-language bots.
 
@@ -33,11 +34,11 @@ def command(languages: tuple, path: str, dev_config: str, nlu: bool, core: bool,
     languages_paths = get_all_languages(path=path, languages=languages)
 
     if nlu:
-        train_nlu(path, languages_paths, dev_config)
+        train_nlu(path, languages_paths, dev_config, force)
     elif core:
-        train_core(path, languages_paths, augmentation, dev_config)
+        train_core(path, languages_paths, augmentation, dev_config, force)
     else:
-        train(path, languages_paths, augmentation, dev_config)
+        train(path, languages_paths, augmentation, dev_config, force)
 
     # print_success("All training tasks completed")
 
@@ -52,39 +53,40 @@ def _inform_language() -> None:
 def get_all_languages(path: str, languages: tuple):
     if len(languages) == 0:
         _inform_language
-        languages_paths = [join(path, 'languages', folder) for folder in os.listdir(join(path, 'languages'))
-                           if os.path.isdir(os.path.join(path, 'languages', folder))]
+        languages_paths = [join(path, "languages", folder) for folder in os.listdir(join(path, "languages"))
+                           if os.path.isdir(os.path.join(path, "languages", folder))]
     else:
-        languages_paths = [join(path, 'languages', folder) for folder in os.listdir(join(path, 'languages'))
-                           if os.path.isdir(os.path.join(path, 'languages', folder)) and folder in languages]
+        languages_paths = [join(path, "languages", folder) for folder in os.listdir(join(path, "languages"))
+                           if os.path.isdir(os.path.join(path, "languages", folder)) and folder in languages]
     return languages_paths
 
 
-def train(path: str, languages_paths: list, augmentation: int, dev_config: str):
+def train(path: str, languages_paths: list, augmentation: int, dev_config: str, force: bool):
     timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
-    stories_path = join(path, 'languages', 'stories.md')
+    stories_path = join(path, "languages", "stories.md")
     for language_path in languages_paths:
         lang = os.path.basename(language_path)  # os.path.split(os.path.dirname(language_path))
         os.system(f"rasa train --config {join(language_path, dev_config)} --domain {join(language_path,'domain.yml')} \
-        --data {join(language_path,'data')} {stories_path} --augmentation {augmentation} \
+        --data {join(language_path,'data')} {stories_path} --augmentation {augmentation} {'--force' if force else ''} \
         --out {join(language_path, 'models')} --fixed-model-name model-{lang}-{timestamp}")
 
 
-def train_nlu(path: str, languages_paths: list, dev_config: str):
+def train_nlu(path: str, languages_paths: list, dev_config: str, force: bool):
     timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
     for language_path in languages_paths:
         lang = os.path.basename(language_path)  # os.path.split(os.path.dirname(language_path))
         os.system(f"rasa train nlu --nlu {join(language_path,'data')} --config {join(language_path, dev_config)} \
+                  {'--force' if force else ''} \
                   --out {join(language_path, 'models')} --fixed-model-name nlu-model-{lang}-{timestamp}")
 
 
-def train_core(path: str, languages_paths: list, augmentation: int, dev_config: str):
+def train_core(path: str, languages_paths: list, augmentation: int, dev_config: str, force: bool):
     timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
     stories_path = join(path, 'languages', 'stories.md')
     for language_path in languages_paths:
         lang = os.path.basename(language_path)  # os.path.split(os.path.dirname(language_path))
         os.system(f"rasa train core --domain {join(language_path,'domain.yml')} --stories {stories_path} \
-                  --augmentation {augmentation} --config {join(language_path, dev_config)} \
+                  --augmentation {augmentation} --config {join(language_path, dev_config)} {'--force' if force else ''} \
                   --out {join(language_path, 'models')} --fixed-model-name core-model-{lang}-{timestamp}")
 
 
