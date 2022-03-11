11abb1f21fbde32141146d236e97ae51d55209c7
Luís Marques <luis_marques73@live.com.pt>
2022-03-11 10:04:30 +0000

    Tests Verification Done and Tested

--- roboai_cli/util/request_reply.py
+++ roboai_cli/util/request_reply.py
@@ -1,6 +1,88 @@
 import json
+import os
 import uuid
 import requests
+
+from roboai_cli.util.input_output import load_yaml
+from deepdiff import DeepDiff
+
+ENDPOINT = "http://rasa-training.development.robo-ai.com:5006"
+REST_PATH = "/webhooks/rest/webhook"
+REST_METHOD = "post"
+META_DATA_PATH = "/status"
+META_DATA_METHOD = "get"
+TRACKER_PATH = "/conversations/{}/tracker?include_events=ALL"
+TRACKER_METHOD = "get"
+
+
+class Tests:
+
+    def __init__(
+            self,
+            endpoint: str,
+            headers: dict,
+            true_files_path: str
+    ) -> None:
+        self.__true_files_list = [true_files_path]
+        self.__message = Message(endpoint=endpoint, headers=headers)
+
+    def run_tests(self):
+        print(self.__message.request_message(META_DATA_PATH, META_DATA_METHOD, body={}))
+        self.get_all_true_files(self.__true_files_list)
+
+    def get_all_true_files(self, paths):
+        while len(paths):
+
+            path = paths.pop()
+            print(path)
+            if os.path.isdir(path):
+                paths = paths + [os.path.join(path, name) for name in os.listdir(path)]
+
+            elif path.endswith(".yml"):
+                self.get_all_stories(load_yaml(path))
+
+    def get_all_stories(self, true_file):
+        list_stories_steps = true_file.get('tests')
+
+        for dict_story_steps in list_stories_steps:
+            id_story = generate_id_story()
+            list_chatbot_reply = []
+            print(dict_story_steps['story'])
+            print(id_story)
+
+            try:
+                for dict_steps in dict_story_steps['steps']:
+                    for dict_steps_key, dict_steps_value in dict_steps.items():
+
+                        if dict_steps_key == "user":
+                            if list_chatbot_reply:
+                                raise Exception(f'The reply was not evaluated: {list_chatbot_reply}')
+
+                            list_chatbot_reply = self.__message.request_message(REST_PATH, REST_METHOD, body={
+                                "sender": id_story,
+                                "message": dict_steps_value
+                            })
+
+                        elif dict_steps_key == "chatbot":
+                            if not list_chatbot_reply:
+                                raise Exception('Chatbot did not reply (the list is empty)')
+
+                            list_chatbot_reply = self.evaluate(list_chatbot_reply, dict_steps_value)
+
+            except Exception as e:
+                print(e)
+                print(self.__message.request_message(TRACKER_PATH.format(id_story), TRACKER_METHOD, body={}))
+                pass
+
+    def evaluate(self, list_chatbot_reply, dict_steps_value):
+        answers_list = list(dict_steps_value.values())[0]
+        chatbot_reply = list_chatbot_reply.pop(0)
+
+        for custom_answers in answers_list:
+            if not DeepDiff(custom_answers['custom'], chatbot_reply['custom']):
+                return list_chatbot_reply
+
+        raise Exception(f"Could not find {chatbot_reply['custom']} on the list {answers_list}")
 
 
 class Message:
@@ -8,86 +90,36 @@ class Message:
     def __init__(
             self,
             endpoint: str,
-            path: str,
-            method: str,
             headers: dict,
-            body: dict,
     ) -> None:
         self.__endpoint = endpoint
-        self.__path = path
-        self.__method = method
         self.__headers = headers
-        self.__body = body
 
-    def request_message(self) -> dict:
+    def request_message(self, path: str, method: str, body: dict) -> dict:
 
         reply_info = None
         reply_info_dict = {}
 
-        if self.__method == "get":
-            reply_info = requests.get(url=f"{self.__endpoint}{self.__path}", headers=self.__headers,
-                                      data=json.dumps(self.__body))
-        elif self.__method == "post":
-            reply_info = requests.post(url=f"{self.__endpoint}{self.__path}", headers=self.__headers,
-                                       data=json.dumps(self.__body))
+        if method == "get":
+            reply_info = requests.get(url=f"{self.__endpoint}{path}", headers=self.__headers,
+                                      data=json.dumps(body))
+        elif method == "post":
+            reply_info = requests.post(url=f"{self.__endpoint}{path}", headers=self.__headers,
+                                       data=json.dumps(body))
 
         if reply_info.status_code == 200:
             reply_info_dict = reply_info.json()
 
         return reply_info_dict
 
-    def set_endpoint(self, endpoint):
-        self.__endpoint = endpoint
 
-    def get_endpoint(self):
-        return self.__endpoint
-
-    def set_path(self, path):
-        self.__path = path
-
-    def get_path(self):
-        return self.__path
-
-    def set_method(self, method):
-        self.__method = method
-
-    def get_method(self):
-        return self.__method
-
-    def set_headers(self, headers_dict):
-        self.__headers = headers_dict
-
-    def set_headers_key(self, key, value):
-        self.__headers[key] = value
-
-    def get_headers(self):
-        return self.__headers
-
-    def get_headers_key(self, key):
-        if key in self.__headers:
-            return self.__headers[key]
-
-    def set_body(self, body_dict):
-        self.__body = body_dict
-
-    def set_body_key(self, key, value):
-        self.__body[key] = value
-
-    def get_body(self):
-        return self.__body
-
-    def get_body_key(self, key):
-        if key in self.__body:
-            return self.__body[key]
-
-
-def generate_id_conversation():
+def generate_id_story():
     return uuid.uuid4().hex
 
 
 if __name__ == "__main__":
-
-    sender = generate_id_conversation()
+    """
+    sender = generate_id_story()
     conversation = Message(
         endpoint="http://rasa-training.development.robo-ai.com:5006",
         path="/webhooks/rest/webhook",
@@ -100,8 +132,11 @@ if __name__ == "__main__":
     )
 
     print(conversation.request_message())
+    """
+    tests = Tests(
+        true_files_path="../roboai_tests/tests_true_files/",
+        endpoint=ENDPOINT,
+        headers={}
+    )
 
-
-
-
-
+    tests.run_tests()
