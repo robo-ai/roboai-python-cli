340ea8f4681a5b07229c6c7202fe1da69aa9177a
Fábio Samina <fabiosamina@gmail.com>
2022-03-21 11:55:18 +0000

    FIX UX: "⠙ Creating true files...No language was provided but a bot was detected.Will test all available languages inside provided bot folder."

--- roboai_cli/commands/create_tests.py
+++ roboai_cli/commands/create_tests.py
@@ -16,22 +16,23 @@ TEST_FOLDER_NAME = os.path.join("roboai_tests", TEMPLATES_FOLDER_NAME)
 @click.option("--template", default=None, type=list, help="Specifies the template name")
 def command(languages: tuple, domain_path: str, templates_path: tuple, template: str):
     if not domain_path and not templates_path:
-        with loading_indicator("Creating true files..."):
-            if exists(join(abspath("."), "languages")):
-                list_domain_dir = get_all_languages(path=abspath("."), languages=languages)
+        if exists(join(abspath("."), "languages")):
+            list_domain_dir = get_all_languages(path=abspath("."), languages=languages)
 
-                while list_domain_dir:
-                    lang_domain_dir = list_domain_dir.pop()
-                    template_dir = [join(lang_domain_dir, TEST_FOLDER_NAME)]
+            while list_domain_dir:
+                lang_domain_dir = list_domain_dir.pop()
+                template_dir = [join(lang_domain_dir, TEST_FOLDER_NAME)]
 
-                    if paths_exist(lang_domain_dir, template_dir): # and not len([template_dir])
+                if paths_exist(lang_domain_dir, template_dir): # and not len([template_dir])
+                    with loading_indicator("Creating true files..."):
                         automate(lang_domain_dir, template_dir)
 
-            else:
-                domain_dir = abspath(".")
-                template_dir = [join(abspath("."), TEST_FOLDER_NAME)]
+        else:
+            domain_dir = abspath(".")
+            template_dir = [join(abspath("."), TEST_FOLDER_NAME)]
 
-                if paths_exist(domain_dir, template_dir):
+            if paths_exist(domain_dir, template_dir):
+                with loading_indicator("Creating true files..."):
                     automate(domain_dir, template_dir)
 
         print_success("Tests created successfully")
@@ -75,7 +76,7 @@ def _inform_language() -> None:
     Auxiliary method to inform the user no languages were passed when executing the test command.
     """
     print_info(
-        "No language was provided but a bot was detected."
+        "No language was provided but a bot was detected.\n"
         "Will test all available languages inside provided bot folder.\n"
     )
 
